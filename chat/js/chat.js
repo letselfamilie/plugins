3cd639d7ead97605ad87658eb5c24cdafdd44756
San Nguyen <mr.sn5.kma@gmail.com>
2019-04-16 17:38:23 +0300

    chat adaptiveness fixed

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -11,6 +11,8 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+
 $(function () {
     console.log('HELLO!');
 
