cd94f3fed5f5dab977eee73cf9f57d8d4716ffe0
San Nguyen <mr.sn5.kma@gmail.com>
2019-04-22 13:19:56 +0300

    check

--- chat/index.php
+++ chat/index.php
@@ -50,11 +50,18 @@ function chat_scripts()
 {
     if (is_page('chat')) {
 
-        wp_register_script('chat-js', plugins_url('js/compiled/chat.js', __FILE__), array('jquery'), date("h:i:s"), true);
-        //wp_register_style('chat-css', plugins_url('less/chat.less', __FILE__), '', date("h:i:s"), 'screen');
 
-        wp_enqueue_script('chat-js');
+        //wp_register_style('chat-css', plugins_url('less/chat.less', __FILE__), '', date("h:i:s"), 'screen');
         //wp_enqueue_style('chat-css');
+
+
+//        wp_enqueue_style( 'bootstrap', plugins_url('css/bootstrap.css', __FILE__), array(), date("h:i:s") );
+//        wp_enqueue_script( 'bootstrap', plugins_url('js/bootstrap.min.js', __FILE__), array('jquery'), date("h:i:s"), true );
+
+
+        wp_register_script('chat-js', plugins_url('js/compiled/chat.js', __FILE__), array('jquery'), date("h:i:s"), true);
+        wp_enqueue_script('chat-js');
+
 
         wp_dequeue_style( 'aurum-main-css' );
 
--- chat/templates/chat.php
+++ chat/templates/chat.php
@@ -24,8 +24,8 @@ $consultant = false;
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,600,700,300' rel='stylesheet' type='text/css'>
 
     <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
-    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 
 
     <?php wp_head(); ?>
