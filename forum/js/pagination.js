aa89b748320ffa39198a8d79457833720dde04ee
San Nguyen <mr.sn5.kma@gmail.com>
2019-04-23 02:13:06 +0300

    fix

--- forum/js/pagination.js
+++ forum/js/pagination.js
@@ -55,7 +55,7 @@ module.exports = function(curr_page, max_page, n_pages = 5, updateFunc, paginati
             $n.prev().addClass('active');
             pagination_obj.current_page -= 1;
             updateFunc(pagination_obj.current_page);
-        } else if (current_page > 1) {
+        } else if (pagination_obj.current_page > 1) {
             pagination_obj.pagina_to--;
             pagination_obj.pagina_from--;
             pagination_obj.current_page--;
