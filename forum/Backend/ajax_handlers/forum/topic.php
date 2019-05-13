5ba499d784c102fd0f1fe5782f0cc66a7f27df9e
San Nguyen <mr.sn5.kma@gmail.com>
2019-05-14 00:06:21 +0300

    -

--- forum/Backend/ajax_handlers/forum/topic.php
+++ forum/Backend/ajax_handlers/forum/topic.php
@@ -72,8 +72,8 @@ function get_forum_topics(){
                      ON {$wpdb->prefix}f_topics.topic_id = {$wpdb->prefix}f_posts.topic_id
                      WHERE cat_name = '$cat_name'
                      GROUP BY topic_name, {$wpdb->prefix}f_topics.topic_id
-                     ORDER BY posts_num DESC, {$wpdb->prefix}f_posts.create_timestamp
-                     LIMIT $_POST[per_page], topic_name
+                     ORDER BY posts_num DESC, topic_name
+                     LIMIT $_POST[per_page]
                      OFFSET ". ( $_POST['page_number'] - 1 ) * $_POST['per_page'] .";";
 
         $topics = array();
