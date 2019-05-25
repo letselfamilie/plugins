43648a1b083bb8b06d94ba4d6dc00ede82b35e02
San Nguyen <mr.sn5.kma@gmail.com>
2019-05-25 16:11:47 +0300

    -

--- chat/js/chat-system.js
+++ chat/js/chat-system.js
@@ -97,7 +97,7 @@ function connectSocket() {
             });
             sound.play();
 
-            let from = data.from_login;
+            let from = data.from_username;
             let mess = data.message;
             let dial_id = data.dialog_id;
             let photo = data.photo;
