6bf65662916962a9b5ba81a1c8e2f6726cb84308
San Nguyen <mr.sn5.kma@gmail.com>
2019-05-25 14:46:11 +0300

    chat-box added

--- chat/js/compiled/chat-system.js
+++ chat/js/compiled/chat-system.js
@@ -56,7 +56,6 @@ function connectSocket() {
                 }));
 
                 console.log("Request of creating new dialog has been sent to server");
-                window.location.href = url_object.site_url + "/chat";
 
             } else (alert("Write your issue, please"))
 
@@ -67,6 +66,98 @@ function connectSocket() {
 
         return false;
 
+    };
+
+
+
+    conn.onmessage = function (e) {
+        console.log(e.data);
+        var data = JSON.parse(e.data);
+
+        // if (data.type === "message") {
+        //
+        //     var sound = new Howl({
+        //         src: ['http://178.128.202.94/wp-content/uploads/2019/04/unconvinced.mp3']
+        //     });
+        //     sound.play();
+        //
+        //     let from = data.from;
+        //     let time = data.time;
+        //     let mess = data.message;
+        //     let dial_id = data.dialog_id;
+        //     let is_chat_with_employee = data.is_employee_chat;
+        //
+        //     let key = searchObjKey(mes, dial_id);
+        //
+        //     $("#" + dial_id + " p.preview").text(mess);
+        //     let $node = $("#" + dial_id);
+        //     $node.detach();
+        //     $node.prependTo("#conversations ul");
+        //
+        //     let isRead = "0";
+        //
+        //     if ($node.hasClass("active")) {
+        //
+        //         //adding message in the open chat
+        //         var m = {user_from_id: from, message_body: mess, create_timestamp: time};
+        //         addMes(m, $('.conversation.active').find("img").attr('src'), is_chat_with_employee);
+        //
+        //         isRead = "1";
+        //
+        //         conn.send(JSON.stringify({
+        //             command: 'mark_messages',
+        //             dialog_id: dial_id
+        //         }));
+        //
+        //
+        //         $('.messages ul').children('li').last().focus();
+        //
+        //     } else {
+        //
+        //         if ($node.find(".badge-counter").length === 0) {
+        //             let badge = '<span class="badge badge-counter ml-2">1</span>';
+        //             $(badge).appendTo($node.find(".wrap .meta .name"));
+        //         } else {
+        //             let val = $node.find(".badge-counter").text();
+        //
+        //             $node.find(".badge-counter").text(isNaN(parseInt(val))? 1 : parseInt(val) + 1);
+        //         }
+        //
+        //         $node.find(".badge-counter").removeClass("hidden");
+        //
+        //         // TODO: add badges of new messages + counter to the conversation
+        //
+        //         console.log("Dialog " + dial_id + " has new message");
+        //     }
+        //
+        //
+        //     var new_message = {
+        //         message_id: "" + mes[Object.keys(mes).length - 1].message_id + 1,
+        //         user_from_id: from,
+        //         dialog_id: dial_id,
+        //         is_read: isRead,
+        //         message_body: mess,
+        //         create_timestamp: time
+        //     };
+        //
+        //     mes[key].messages.push(new_message);
+        //
+        //     gotoBottom('messages-container');
+        // }
+
+        if (data.type === "new_chat") {
+            let dialog_id = data.dialog_id;
+            let dialog_type = data.dialog_type; //  employee_chat || user_chat
+            if(dialog_type==="employee_chat")
+            {
+                window.location.href = url_object.site_url + "/chat?dialog_id=" + dialog_id;
+            }
+
+            // var sound = new Howl({
+            //     src: ['http://178.128.202.94/wp-content/uploads/2019/04/unconvinced.mp3']
+            // });
+            // sound.play();
+        }
     };
 }
 },{"ejs":3}],2:[function(require,module,exports){
