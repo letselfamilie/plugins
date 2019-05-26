99d75e0350f6267e4fbc5f4906cd27be9c1479cf
San Nguyen <mr.sn5.kma@gmail.com>
2019-05-27 01:33:26 +0300

    report

--- forum/Backend/ajax_handlers/forum/post.php
+++ forum/Backend/ajax_handlers/forum/post.php
@@ -86,7 +86,7 @@ function add_post_report()
             $messageToSocket['user_login'] = $user_info->user_login;
             $messageToSocket['message_text'] = $post->post_message;
 
-            report_mail_admin('mr.sn5.kma@gmail.com', $post->user_id, $post->post_message);
+            report_mail_admin('mr.sn5.kma@gmail.com', $user_info->user_login, $post->post_message);
             // report_mail_admin('info@letselfamilie.nl', $post->user_id, $post->post_message);
 //                    $conn->on('message', function ($msg) use ($conn) {
 //                        echo "Received: {$msg}\n";
