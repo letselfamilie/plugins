371b9655cb2f862b619dc1812ac2918a376ef532
AnneManzhura <manzhurik@gmail.com>
2019-06-06 01:48:06 +0300

    Redirecting dialogs

--- chat/js/compiled/chat.js
+++ chat/js/compiled/chat.js
@@ -1022,21 +1022,13 @@ function addMes(item, user2logo, is_employee_chat, prepend) {
     var addBanner = true;
 
     var lastMes = $(".messages ul li.mes").last();
-
-    //$("li.someclass", rootOfList).last()
-
     //var lastMes = $(".messages ul li.mes:last-child");
+
     let date_reg = new RegExp('(.|\\s)*(\\d{4}-\\d{2}-\\d{2})(.|\\s)*');
     let date = lastMes.prop('outerHTML');
-
-    console.log("lastMes: "+date);
-    if(date===undefined)
-    {
-        console.log("undefined");
-    }
-
     
-    else{
+
+    if(date!==undefined){
         date = date.replace(date_reg, '$2');
         var parts2 =date.split(' ')[0].split('-');
         var dateOfPrevMes = new Date(parts2[0], parts2[1] - 1, parts2[2]);
