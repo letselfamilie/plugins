2ba7c83a6b1ed7032d9c97be3027de6a9530bb05
San Nguyen <mr.sn5.kma@gmail.com>
2019-06-10 00:20:53 +0300

    -

--- chat/index.php
+++ chat/index.php
@@ -85,7 +85,7 @@ function chat_scripts()
     wp_localize_script('chat-system-js', 'wp_object',
         array('plugin_directory' => plugins_url('', __FILE__),
             'is_post' => is_page('posts'),
-            'is_chat' => is_page('chat'),
+            'is_chat' => (is_page('chat')) ? is_page('chat'): 0,
             'is_reg' => is_page('register')));
 
     $current_user = wp_get_current_user();
